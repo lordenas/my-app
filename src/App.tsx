e84b8f535b5ce65bc44a342c1484a56dad163783
Sergey <lordenas@gmail.com>
2021-09-30 13:03:05 +0300

    add provide

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { store } from './store/configureStore';
+import { Provider } from 'react-redux';
 
 function App() {
 	return (
-		{/* <Provider store={store}>
+		<Provider store={store}>
 			<div className="App">
-
+				test
 			</div>
-		</Provider> */}
+		</Provider>
 	);
 }
 
